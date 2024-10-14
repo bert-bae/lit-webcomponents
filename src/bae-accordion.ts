afabb6888bba8c1842f22f33f7cc18a32a4ab865
Elbert B. <elbert.bae@gmail.com>
2024-10-14 14:04:31 -0700

    Setup accordion with toggle to open and close

--- src/bae-accordion.ts
+++ src/bae-accordion.ts
@@ -1,26 +1,48 @@
-import { LitElement, css, html } from 'lit'
-import { customElement, property } from 'lit/decorators.js'
+import { LitElement, css, html } from "lit";
+import { customElement, property } from "lit/decorators.js";
+import { when } from "lit/directives/when.js";
 
-@customElement('bae-accordion')
+@customElement("bae-accordion")
 export class BaeAccordion extends LitElement {
+  static override styles = css`
+    :host {
+      border: 1px solid red;
+      width: 100%;
+      display: block;
+      padding: 8px;
+    }
+
+    :host h3 {
+      margin: 0;
+      padding: 0;
+    }
+  `;
 
   @property({ type: String })
-  title = ''
+  title = "";
+
+  @property({ type: Boolean })
+  open = false;
+
+  toggleView(e: MouseEvent) {
+    e.stopPropagation();
+    this.open = !this.open;
+  }
 
   render() {
     return html`
       <div>
-        <div class="accordion-title">
+        <div class="accordion-title" @click="${this.toggleView}">
           <h3>${this.title}</h3>
         </div>
-        <slot></slot>
+        ${when(this.open, () => html`<slot></slot>`)}
       </div>
-    `
+    `;
   }
 }
 
 declare global {
   interface HTMLElementTagNameMap {
-    'bae-accordion': BaeAccordion
+    "bae-accordion": BaeAccordion;
   }
 }
