2d6a914f9992b214c2b248c6a75cf8793e9d3a85
Elbert B. <elbert.bae@gmail.com>
2024-10-14 14:55:56 -0700

    Simple styling experiment for slot

--- src/bae-accordion.ts
+++ src/bae-accordion.ts
@@ -6,15 +6,35 @@ import { when } from "lit/directives/when.js";
 export class BaeAccordion extends LitElement {
   static override styles = css`
     :host {
-      border: 1px solid red;
       width: 100%;
       display: block;
+    }
+
+    :host .accordion-paper {
+      box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.25);
+    }
+
+    :host .accordion-title {
+      cursor: pointer;
+      background-color: black;
+      color: white;
+      border-radius: 4px;
       padding: 8px;
     }
 
     :host h3 {
       margin: 0;
       padding: 0;
+    }
+
+    :host .accordion-content {
+      height: 0;
+      transition: 0.2s all ease-in;
+      &[data-open="true"] {
+        height: 150px;
+        padding: 8px;
+        overflow-y: auto;
+      }
     }
   `;
 
@@ -31,11 +51,14 @@ export class BaeAccordion extends LitElement {
 
   render() {
     return html`
-      <div>
+      <div class="accordion-paper">
         <div class="accordion-title" @click="${this.toggleView}">
           <h3>${this.title}</h3>
         </div>
-        ${when(this.open, () => html`<slot></slot>`)}
+
+        <div class="accordion-content" data-open="${this.open}">
+          ${when(this.open, () => html`<slot></slot>`)}
+        </div>
       </div>
     `;
   }
